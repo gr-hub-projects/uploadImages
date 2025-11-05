4c655852fef20a94487148155673828084c9bd63
Denis <163061220+sdenisg@users.noreply.github.com>
2025-11-05 11:04:40 -0500

    Add js

---
+++ Hacienda_del_mar/JS/section.js
@@ -0,0 +1,19 @@
+$('.habitaciones-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    responsive: [
+    {
+
+        breakpoint: 990,
+        arrows: false,
+        settings: { slidesToShow: 1.4 },
+
+        breakpoint: 768,
+        arrows: false,
+        settings: { slidesToShow: 1 }
+    }
+    ]
+});
